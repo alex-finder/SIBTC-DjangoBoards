5d9fe0b509531d013b8427af7ff14ed520b95321
Alex Finder <alexandru.finder@yahoo.com>
2023-01-06 00:49:35 +0200

    Added functionality for extracting the current logged in user on posting topics

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpRequest, Http404
 from django.contrib.auth.decorators import login_required
-from .models import Board, Topic, Post
+
 from .forms import NewTopicForm
+from .models import Board, Topic, Post
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -49,18 +50,19 @@ def board_topics(request: HttpRequest, pk: int) -> HttpResponse:
 @login_required
 def new_topic(request: HttpRequest, pk: int) -> HttpResponse:
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()
+    # user = User.objects.first()
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            # topic.starter = user
+            topic.starter = request.user
             topic.save()
             post = Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
